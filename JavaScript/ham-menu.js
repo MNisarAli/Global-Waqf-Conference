46dead6334e6abb74715cabd4c89d45769c21421
M.Nisar Ali <dr.nisaralig@gmail.com>
2023-04-17 16:34:31 +0500

    Add body class to disable scrolling when menu is open

--- JavaScript/ham-menu.js
+++ JavaScript/ham-menu.js
@@ -1,12 +1,14 @@
 // Select hamburger button and navigation menu.
 const hamburgerButton = document.querySelector('.hamburger');
 const navigationMenu = document.querySelector('#navigation-menu');
+const body = document.querySelector('body');
 
 // Toggle the navigation menu when the hamburger button is clicked.
 hamburgerButton.addEventListener('click', () => {
   const expanded = hamburgerButton.getAttribute('aria-expanded') === 'true' || false;
   hamburgerButton.setAttribute('aria-expanded', !expanded);
   navigationMenu.classList.toggle('active');
+  body.classList.toggle('active');
 });
 
 // Close the navigation menu when a link is clicked.
@@ -14,4 +16,5 @@ const links = document.querySelectorAll('.nav-item');
 links.forEach((link) => link.addEventListener('click', () => {
   hamburgerButton.setAttribute('aria-expanded', false);
   navigationMenu.classList.remove('active');
+  body.classList.remove('active');
 }));
